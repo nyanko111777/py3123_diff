7fcc46a30aeaac1e99b35d60b286aad9885f3f21
myao9494 <mineouka@outlook.jp>
2025-12-24 07:05:35 +0900

    del

--- diff_py3123/b07bdedd_kOL4.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://github.com/nyanko111777');
-  await page.getByRole('link', { name: 'VS_Code_setting_myao', exact: true }).click();
-});
